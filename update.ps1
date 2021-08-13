3bcc50dc6dea84205a2655cd33f71a6d618c5713
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-08-13 13:42:10 -0700

    Fix auto-updating
    - Now gets RCS spec files as well
    - Workflow updated
    - tool/ removed

--- update.ps1
+++ update.ps1
@@ -1,143 +1,170 @@
-# FORMAT JSON
-# Formats JSON in a nicer format than the built-in ConvertTo-Json does.
-# https://github.com/PowerShell/PowerShell/issues/2736
-function Format-Json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {
-    $indent = 0;
-    ($json -Split "`n" | ForEach-Object {
-        if ($_ -match '[\}\]]\s*,?\s*$') {
-            # This line ends with ] or }, decrement the indentation level
-            $indent--
-        }
-        $line = ('  ' * $indent) + $($_.TrimStart() -replace '":  (["{[])', '": $1' -replace ':  ', ': ')
-        if ($_ -match '[\{\[]\s*$') {
-            # This line ends with [ or {, increment the indentation level
-            $indent++
-        }
-        $line
-    }) -Join "`n"
+#requires -PSEdition Core
+
+# Config.
+$OUT_DIR = 'out'
+$RCS_OUT_DIR = "$OUT_DIR\rcs"
+$LCU_OUT_DIR = "$OUT_DIR\lcu"
+
+$LOGIN_FILE = 'lollogin.json'
+$LOGIN = Get-Content $LOGIN_FILE | ConvertFrom-Json
+
+$RCS_LOCKFILE = "$env:LOCALAPPDATA\Riot Games\Riot Client\Config\lockfile"
+$LCU_DIR = 'C:\Riot Games\League of Legends'
+$LCU_LOCKFILE = "$LCU_DIR\lockfile"
+$LCU_SYSTEMYAML = "$LCU_DIR\system.yaml"
+$LCU_EXE = "$LCU_DIR\LeagueClient.exe"
+
+If (-Not (Test-Path $LOGIN_FILE)) {
+    Throw "Login not found: $LOGIN_FILE."
 }
 
-# IGNORE SSL ERRORS.
-Add-Type @"
-    using System.Net;
-    using System.Security.Cryptography.X509Certificates;
-    public class TrustAllCertsPolicy : ICertificatePolicy {
-        public bool CheckValidationResult(
-            ServicePoint srvPoint, X509Certificate certificate,
-            WebRequest request, int certificateProblem) {
-            return true;
-        }
-    }
-"@
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+function Stop-RiotProcesses {
+    # Stop any existing processes.
+    Stop-Process -Name 'RiotClientUx' -ErrorAction Ignore
+    Stop-Process -Name 'LeagueClient' -ErrorAction Ignore
+    Remove-Item $RCS_LOCKFILE -Force -ErrorAction Ignore
+    Remove-Item $LCU_LOCKFILE -Force -ErrorAction Ignore
+}
 
-$OUT_DIR = 'out'
-
-# Stop any existing processes.
-Stop-Process -Name "LeagueClient" -ErrorAction Ignore
-# Create out folder
-New-Item -ItemType Directory -Force -Path $OUT_DIR | Out-Null
-
-
-# LCU SCRIPT
-$LEAGUE_DIR = 'C:\Riot Games\League of Legends'
-$LOCK_FILE = "$LEAGUE_DIR\lockfile"
-$YAML_FILE = "$LEAGUE_DIR\Config\lcu-schema\system.yaml"
-$RIOT_USERNAME = 'riot'
-
-# RIOT GAMES USER ACCOUNT
-$LOLLOGIN_FILE = 'lollogin.json'
-
-Write-Host 'Creating alternate system.yaml.'
-New-Item -Path "$LEAGUE_DIR\Config\lcu-schema\system.yaml" -Force | Out-Null
-$systemYaml  = Get-Content "$LEAGUE_DIR\system.yaml"
-$systemYaml  = $systemYaml[0..($systemYaml.count - 4)]
-$systemYaml += ('enable_swagger: true')
-$systemYaml | Out-File $YAML_FILE -Encoding ascii
-
-Write-Host 'Starting LeagueClient.exe.'
-& "$LEAGUE_DIR\LeagueClient.exe" "--system-yaml-override=$LEAGUE_DIR\Config\lcu-schema\system.yaml"
-
-
-function Invoke-LcuRequest {
+function Invoke-RiotRequest {
     Param (
+        [Parameter(Mandatory=$true)]  [String]$lockfile,
         [Parameter(Mandatory=$true)]  [String]$path,
         [Parameter(Mandatory=$false)] [String]$method = 'GET',
-        [Parameter(Mandatory=$false)] $body = $null
+        [Parameter(Mandatory=$false)] $body = $null,
+        [Parameter(Mandatory=$false)] [Int]$attempts = 100
     )
 
-    Start-Sleep 1
-    $attempt = 100
-    While ($true) {
+    While ($True) {
         Try {
-            $lockContent = Get-Content $LOCK_FILE -Raw
+            $lockContent = Get-Content $lockfile -Raw
             $lockContent = $lockContent.Split(':')
             $port = $lockContent[2];
             $pass = $lockContent[3];
 
-            $userpass = "${RIOT_USERNAME}:$pass"
-            $userpass = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($userpass))
-            Write-Host "Lockfile parsed, target ':$port$path', auth basic: '$userpass', left: $attempt."
+            $pass = ConvertTo-SecureString $pass -AsPlainText -Force
+            $cred = New-Object -TypeName PSCredential -ArgumentList 'riot', $pass
 
-            $result = Invoke-RestMethod `
-                -Uri "https://127.0.0.1:$port$path" `
-                -Headers @{ 'Authorization' = "Basic $userpass" } `
-                -ContentType 'application/json' `
-                -Body $($body | ConvertTo-Json) `
+            $result = Invoke-RestMethod "https://127.0.0.1:$port$path" `
+                -SkipCertificateCheck `
                 -Method $method `
-                -UseBasicParsing
+                -Authentication 'Basic' `
+                -Credential $cred `
+                -ContentType 'application/json' `
+                -Body $($body | ConvertTo-Json)
             Return $result
         } Catch {
-            $attempt--
-            If ($attempt -le 0) {
-                Write-Host 'Failed to get data from the LCU'
+            $attempts--
+            If ($attempts -le 0) {
+                Write-Host "Failed to $method '$path'."
                 Throw $_
             }
-            Write-Host "Failed to get data, retrying: $_"
+            Write-Host "Failed to $method '$path', retrying: $_"
             Start-Sleep 5
         }
     }
 }
 
-Try {
-    # Help behaves weird if you call it after logging in: returns a JSON-serialized string with duplicate keys.
-    Write-Host 'Getting help.'
-    Invoke-LcuRequest '/help' | ConvertTo-Json -Depth 100 | Format-Json | Out-File -Encoding UTF8 "$OUT_DIR\help.json"
+# Stop any existing processes.
+Stop-RiotProcesses
 
-    # If login info available, log-in first to ensure LCU is updated.
-    If (Test-Path $LOLLOGIN_FILE) {
-        Write-Host "$LOLLOGIN_FILE found, attempting to log-in."
-        $login = Get-Content $LOLLOGIN_FILE | ConvertFrom-Json
-        Invoke-LcuRequest '/lol-login/v1/session' 'POST' $login
-        Write-Host 'Logged in.'
+# Create output folders.
+New-Item -ItemType Directory -Force -Path $OUT_DIR | Out-Null
+New-Item -ItemType Directory -Force -Path $RCS_OUT_DIR | Out-Null
+New-Item -ItemType Directory -Force -Path $LCU_OUT_DIR | Out-Null
 
-        Write-Host 'Getting queues.'
-        Invoke-LcuRequest '/lol-game-queues/v1/queues' |
-            ConvertTo-Json -Depth 100 | Format-Json | Out-File -Encoding UTF8 "$OUT_DIR\queues.json"
+# Install League if not installed.
+If (-Not (Test-Path $LCU_EXE)) {
+    Write-Host 'Installing LoL.'
 
-        Write-Host 'Getting maps.'
-        Invoke-LcuRequest '/lol-maps/v1/maps' |
-            ConvertTo-Json -Depth 100 | Format-Json | Out-File -Encoding UTF8 "$OUT_DIR\maps.json"
+    Invoke-WebRequest 'https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe' -OutFile 'install.na.exe'
+    .\install.na.exe --skip-to-install
 
-        Write-Host 'Getting store-catalog.'
-        Invoke-LcuRequest '/lol-store/v1/catalog' |
-            ConvertTo-Json -Depth 100 | Format-Json | Out-File -Encoding UTF8 "$OUT_DIR\store-catalog.json"
+    $attempts = 20
+    While ($True) {
+        $status = Invoke-RiotRequest $RCS_LOCKFILE '/patch/v1/installs/league_of_legends.live/status'
+        If ('up_to_date' -Eq $status.patch.state) {
+            Break
+        }
+        Write-Host "Installing LoL: $($status.patch.progress.progress)%"
+
+        If ($attempts -le 0) {
+            Throw 'Failed to install LoL.'
+        }
+        $attempts--
+        Start-Sleep 20
     }
-    Else {
-        Write-Host "$LOLLOGIN_FILE not found, not getting data requiring log-in."
-    }
-
-    Write-Host 'Getting spec.'
-    $specResponse = Invoke-LcuRequest '/swagger/v3/openapi.json'
-    $specResponse | ConvertTo-Json -Depth 100 | Format-Json | Out-File -Encoding UTF8 "$OUT_DIR\openapi.json"
-    $specResponse | ConvertTo-Json -Depth 100 | Format-Json | Out-File -Encoding UTF8 "$OUT_DIR\spec.json"
-    $specResponse | ConvertTo-Json -Depth 100 -Compress | Out-File -Encoding UTF8 "$OUT_DIR\spec.min.json"
-
-    Invoke-LcuRequest '/swagger/v2/swagger.json' | ConvertTo-Json -Depth 100 | Format-Json |
-        Out-File -Encoding UTF8 "$OUT_DIR\swagger.json"
-} Finally {
-    Stop-Process -Name 'LeagueClient'
-    Remove-Item $LOCK_FILE
+    Write-Host 'LoL installed successfully.'
+    Stop-RiotProcesses
+}
+Else {
+    Write-Host 'LoL already installed.'
 }
 
-Write-Host 'Success.'
+# Start RCS.
+Write-Host 'Starting RCS.'
+& $LCU_EXE
+
+Start-Sleep 5 # Wait for RCS to load so it doesn't overwrite system.yaml.
+
+# Setup system.yaml.
+$ENABLE_SWAGGER = 'enable_swagger: true';
+If (Select-String -Path $LCU_SYSTEMYAML -Pattern $ENABLE_SWAGGER -SimpleMatch -Quiet) {
+    Write-Host "System.yaml already has '$ENABLE_SWAGGER'."
+}
+Else {
+    Write-Host "Updating system.yaml with '$ENABLE_SWAGGER'."
+    $systemYaml = Get-Content $LCU_SYSTEMYAML
+    $systemYaml += ($ENABLE_SWAGGER)
+    $systemYaml | Out-File $LCU_SYSTEMYAML -Encoding ascii
+}
+
+Try {
+    Start-Sleep 5
+
+    # RCS files.
+    Write-Host 'Getting RCS spec files.'
+    Invoke-RiotRequest $RCS_LOCKFILE '/Help'                    | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 "$RCS_OUT_DIR\help.json"
+    Invoke-RiotRequest $RCS_LOCKFILE '/swagger/v3/openapi.json' | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 "$RCS_OUT_DIR\openapi.json"
+    Invoke-RiotRequest $RCS_LOCKFILE '/swagger/v2/swagger.json' | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 "$RCS_OUT_DIR\swagger.json"
+
+    # Login to RCS to start the LCU.
+    Write-Host 'Logging into RCS, starts LCU.'
+    Invoke-RiotRequest $RCS_LOCKFILE '/rso-auth/v1/authorization/gas' 'POST' $LOGIN | Out-Null
+
+    # Wait for LCU to update itself.
+    Start-Sleep 5
+    Invoke-RiotRequest $LCU_LOCKFILE '/lol-patch/v1/products/league_of_legends/state' # Burn first request.
+    Start-Sleep 10
+    $attempts = 40
+    While ($True) {
+        $state = Invoke-RiotRequest $LCU_LOCKFILE '/lol-patch/v1/products/league_of_legends/state'
+        If ('Idle' -Eq $state.action) {
+            Break
+        }
+
+        Write-Host "LCU updating: $($state.action)" # Not that useful.
+
+        If ($attempts -le 0) {
+            Throw 'LCU failed to update.'
+        }
+        $attempts--
+        Start-Sleep 20
+    }
+
+    # LCU files.
+    Write-Host 'Getting LCU queues, maps, catalog.'
+    Invoke-RiotRequest $LCU_LOCKFILE '/lol-game-queues/v1/queues' | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 "$LCU_OUT_DIR\queues.json"
+    Invoke-RiotRequest $LCU_LOCKFILE '/lol-maps/v1/maps'          | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 "$LCU_OUT_DIR\maps.json"
+    Invoke-RiotRequest $LCU_LOCKFILE '/lol-store/v1/catalog'      | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 "$LCU_OUT_DIR\catalog.json"
+
+    Write-Host 'Getting LCU spec files.'
+    # /Help is missing the `Content-Type: application/json` header when logged-in.
+    Invoke-RiotRequest $LCU_LOCKFILE '/Help' | ConvertFrom-Json -AsHashTable | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 "$LCU_OUT_DIR\help.json"
+    Invoke-RiotRequest $LCU_LOCKFILE '/swagger/v3/openapi.json' | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 "$LCU_OUT_DIR\openapi.json"
+    Invoke-RiotRequest $LCU_LOCKFILE '/swagger/v2/swagger.json' | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 "$LCU_OUT_DIR\swagger.json"
+} Finally {
+    Stop-RiotProcesses
+}
+
+Write-Host 'Success!'
